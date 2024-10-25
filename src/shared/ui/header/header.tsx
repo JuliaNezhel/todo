4101302bbbf9dd95404cb989a548fd7ecac6deb7
JuliaNezhel <yulyazelikova@gmail.com>
2024-10-25 14:29:36 +0300

    refactor: refactor header

--- src/shared/ui/header/header.tsx
+++ src/shared/ui/header/header.tsx
@@ -1,25 +1,45 @@
-import style from './header.module.scss'
+import { NavLink, NavLinkRenderProps } from "react-router-dom";
+import style from "./header.module.scss";
+import { ROUTES } from "../../enums/pathRoutes";
 
-export const Header = () => {
+
+type Props = {
+  buttonTitle?: string
+}
+
+export const Header = ({buttonTitle}: Props) => {
+  const styleLink = ({ isActive }: NavLinkRenderProps): string | undefined => {
+    return isActive ? `${style.link__isActive} ${style.link}` : style.link;
+  };
   return (
     <header className={style.header}>
       <a className={style.logo}>ToDo List</a>
       <nav>
         <ul className={style.navList}>
-            <li>
-                <a  className={style.link}>Задачи</a>
-            </li>
-            <svg width="2" height="28" viewBox="0 0 2 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect width="2" height="28" fill="#DBE2EF"/>
-</svg>
+          <li>
+            <NavLink to={ROUTES.TASKS} className={styleLink}>
+              Задачи
+            </NavLink>
+          </li>
+          <svg
+            width="2"
+            height="28"
+            viewBox="0 0 2 28"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="2" height="28" fill="#DBE2EF" />
+          </svg>
 
-            <li>
-                <a className={style.link}>Категории</a>
-            </li>
+          <li>
+            <NavLink to={ROUTES.CATEGORY} className={styleLink}>
+              Категории
+            </NavLink>
+          </li>
         </ul>
       </nav>
 
-      <button className={style.button}>Добавить задачу</button>
+      <button className={style.button}>{buttonTitle}</button>
     </header>
   );
 };
